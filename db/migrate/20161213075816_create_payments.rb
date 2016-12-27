3bf1b9558070907a084c84f06348e10d808e04a6
oseifrimpong <obedosei@gmail.com>
2016-12-28 06:22:07 +0800

    Payment Migration

--- db/migrate/20161213075816_create_payments.rb
+++ db/migrate/20161213075816_create_payments.rb
@@ -1,7 +1,14 @@
 class CreatePayments < ActiveRecord::Migration[5.0]
   def change
     create_table :payments do |t|
-    
+    	t.string :username, null: false 
+    	t.string :firstname, null: false
+    	t.string :lastname, null: false
+    	t.integer :phonenumber
+    	t.number :accountnumber
+    	t.string :email
+    	t.string :currency
+    	t.decimal :amount
       t.timestamps
     end
   end
