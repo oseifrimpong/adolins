eaf25fc46a2ee0f964df88a38535b1dff2f5eb27
oseifrimpong <obedosei@gmail.com>
2017-02-06 11:42:05 +0800

    fixing papareclip error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ApplicationRecord
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  #validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  validates_attachment_file_name :image, matches: [/png\z/, /jpeg\z/]
+  #validates_attachment_file_name :image, matches: [/png\z/, /jpeg\z/]
+
+  do_not_validate_attachment_file_type :image
 
 
 	validates :title, presence: true  
--- config/application.rb
+++ config/application.rb
@@ -21,3 +21,17 @@ module Adolins
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+# Allow ".foo" as an extension for files with the MIME type "text/plain".
+Paperclip.options[:content_type_mappings] = {
+  foo: %w(text/plain)
+}
+require 'paperclip/media_type_spoof_detector'
+
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
