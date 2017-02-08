a69c2f64cb48de6695b84bf3be8fb70faf762866
oseifrimpong <obedosei@gmail.com>
2017-02-08 16:29:45 +0800

    back button fixed

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  resources :orders
-  resources :payments
+ 
   #resources :payment, only: [:new, :create, :show]
   
   root to: 'posts#index'
