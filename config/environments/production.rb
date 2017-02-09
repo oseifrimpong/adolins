8a00697cdeb5e2d37755766e78ef8922d739f89f
oseifrimpong <obedosei@gmail.com>
2017-02-09 20:59:39 +0800

    slow loading fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.compile = true
   config.public_file_server.enabled = true
 
-  config.assets.precompile += %w[active_admin.css active_admin.js]
+  #config.assets.precompile += %w[active_admin.css active_admin.js]
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
